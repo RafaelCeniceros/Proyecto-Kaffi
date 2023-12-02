fda52442404e0dfadd03a151d84b06c29f306989
Oswaldo Fonseca <soy.oswaldofonseca@Gmail.com>
2023-12-02 02:37:35 -0600

    Form's validation with script

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,25 +1,67 @@
-const botonHistoria = document.getElementById("button-history");
+console.log("contact page");
 
-const botonValores = document.getElementById("button-values");
+//Referencia del formulario de contacto
+const contactForm = document.forms["contact-form"];
 
-const textoHistoria = document.getElementById("texto-historia");
+contactForm.addEventListener ("submit",(event) =>{
+    event.preventDefault();
+    console.log(event);
 
-const textoValores = document.getElementById("texto-values");
+    const user = {
+        name : contactForm.elements["Name-Imput"].value,
+        email : contactForm.elements["E-Mail-Imput"].value,
+        commentary : contactForm.elements["Commentary-Imput"].value
+    };
 
-botonHistoria.addEventListener("click", () => {
-  if (textoHistoria.style.display === "none") {
-    textoHistoria.style.display = "block";
-  } else {
-    textoHistoria.style.display = "none";
-  }
-});
+    if (dataCheckout(user)) {
+        sendData(user);
+    };
+});  
 
-botonValores.addEventListener("click", () => {
-    if (textoValores.style.display === "none") {
-      textoValores.style.display = "block";
+//Validacion de formulario
+const dataCheckout = (user) =>{
+    let response = true;
+    const regex = /^[a-zA-Z\s]+$/;
+    const regex2 = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    
+    if(user.name === ""){
+        errorMessage("Introdusca un nombre valido");
+        response = false;
+    } else if (user.name.length > 25 || user.name.length < 2 ){
+        errorMessage("Su nombre debe tener entre 2 y 25 caracteres");
+        response = false;
+    } else if (!regex.test(user.name)){
+        errorMessage("Tu nombre solo puede contener letras y espacios");
+        response = false;
+    } else if (!regex2.test(user.email)){
+        errorMessage("introdusca un correo electronico valido");
+        response = false;
+    } else if (user.commentary.length < 10 || user.commentary.length > 120 ){
+        errorMessage("Tu comentario deve tener entre 10 y 120 caracteres");
+        response = false;
     } else {
-      textoValores.style.display = "none";
+        errorMessage("");
     }
-  });
+
+    return response;
+};
 
 
+//Imprimir mensaje de error en DOM
+const errorMessage = (message) => {
+    console.log(message);
+    const errorMessage = document.getElementById("error-message");
+    const errorMessageContainer = document.getElementById ("error-message-container");
+
+    errorMessage.innerHTML = message;
+    if (message === ""){
+        errorMessageContainer.style.display = "none";   
+    }else { errorMessageContainer.style.display = "block";
+    }
+};
+
+
+//Envio de datos a correo electronico o a API
+const sendData = ( user ) => {
+    console.table(user);
+};
