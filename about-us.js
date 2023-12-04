365e1add14cc903b215037b4d42d30fcd9026729
Rafael Ceniceros <rrafa7114@gmail.com>
2023-12-03 20:06:08 -0600

    final changes

--- about-us.js
+++ about-us.js
@@ -1,25 +1,28 @@
 const botonHistoria = document.getElementById("button-history");
-
 const botonValores = document.getElementById("button-values");
+const textoHistoria = document.getElementById("history-text");
+const textoValores = document.getElementById("values-text");
 
-const textoHistoria = document.getElementById("texto-historia");
-
-const textoValores = document.getElementById("texto-values");
+// Ocultar el texto al principio
+textoHistoria.style.display = "none";
+textoValores.style.display = "none";
 
 botonHistoria.addEventListener("click", () => {
   if (textoHistoria.style.display === "none") {
     textoHistoria.style.display = "block";
+    botonHistoria.style.display = "none";
   } else {
     textoHistoria.style.display = "none";
+    botonHistoria.style.display = "block";
   }
 });
 
 botonValores.addEventListener("click", () => {
-    if (textoValores.style.display === "none") {
-      textoValores.style.display = "block";
-    } else {
-      textoValores.style.display = "none";
-    }
-  });
-
-
+  if (textoValores.style.display === "none") {
+    textoValores.style.display = "block";
+    botonValores.style.display = "none";
+  } else {
+    textoValores.style.display = "none";
+    botonValores.style.display = "block";
+  }
+});
