7394fd7cc4eab61cbd9173edc3a9564292fbcb55
shinratensei-sudo <fernandojs459@gmail.com>
2023-12-05 21:30:43 -0600

    carpeta con archivos para probar la clase Products

---
+++ assets/js/prueba-clase-products/product-class.js
@@ -0,0 +1,91 @@
+const getProductsUsingFetch = async (url) => {
+    await fetch(url)
+        .then((response) => { 
+            return response.json();
+        })
+        .then(products => products.map((element,index,array) => {
+            const objeto = new Producto(
+            element.id,
+            element.nombre,
+            element.categoria,
+            element.precio,
+            element.descripcion,
+            element.imagen
+            )
+            objeto.name = "Cambio de nombre de producto " + index;  // se realiza con setter
+            console.log(objeto.name); // se obtiene con getter
+            })
+        )
+        .catch((error) => {
+            console.log(error);
+
+        })
+} 
+
+getProductsUsingFetch('./lista-productos.json');
+
+class Producto {
+    #id;
+    #name;
+    #category;
+    #price;
+    #description;
+    #image;
+
+    constructor(id, name, category, price, description, image) {
+        this.#id = id;
+        this.#name = name;
+        this.#category = category;
+        this.#price = price;
+        this.#description = description;
+        this.#image = image;
+    }
+
+    // ----------------- getters -----------------------------
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get category() {
+        return this.#category;
+    }
+
+    get price() {
+        return this.#price;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get image() {
+        return this.#image;
+    }
+
+    // ---------------- setters ------------------------------
+    set name(newName) {
+        this.#name = newName;
+    }
+
+    set category(newCategory) {
+        this.#category = newCategory
+    }
+
+    set price(newPrice) {
+        this.#price = newPrice;
+    }
+
+    set description(newDescription) {
+        this.#description = newDescription;
+    }
+
+    set image(newImage) {
+        this.#image = newImage;
+    }
+
+}
+
